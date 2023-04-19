6672a29489dfcdc9ea44ec1e06e06b410689bc10
Saranga <54878558+Saranaga123@users.noreply.github.com>
2023-04-19 10:20:05 +0530

    strict qu false
    strict qu false

--- src/configs/database.config.ts
+++ src/configs/database.config.ts
@@ -1,11 +1,14 @@
-import {connect , ConnectOptions} from "mongoose";
+import { connect, ConnectOptions, set } from "mongoose";
 
-export const dbConnect = () => { 
-    connect(process.env.MONGO_URI!, {
-        useNewUrlParser:true,
-        useUnifiedTopology:true
-    } as ConnectOptions ).then(
-        ()=>console.log("Connected"),
-        (error)=>console.log("DB Connection Error")
-    )
-}
+export const dbConnect = () => {
+  // Set strictQuery option to false to prepare for Mongoose 7
+  set('strictQuery', false);
+
+  connect(process.env.MONGO_URI!, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  } as ConnectOptions).then(
+    () => console.log("Connected"),
+    (error) => console.log("DB Connection Error")
+  );
+};
